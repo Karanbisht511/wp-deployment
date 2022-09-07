1de094d25863c2317a6f5a4ed748003a8b0de251
Karan Bisht <karanbisht511@gmail.com>
2022-09-07 12:24:52 +0530

    server changed

--- server.js
+++ server.js
@@ -13,15 +13,14 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
-app.use(express.static(path.join(__dirname, "build")));
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
